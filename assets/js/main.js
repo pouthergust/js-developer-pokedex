d93645ef8cc90c22bfe5dcda05f4ec86585784b4
Gabriel Henrique <gabriel.henrique@agenciam3.com>
2024-04-15 22:24:38 -0300

    feat: building pokemons details page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,19 @@
+function setUrlHash(hash = '1') {
+  window.location.hash = hash
+}
 
-console.log('Sucesso!')
+function loadPokemonDetails() {
+  const pokemonId = window.location.hash.replace(/[#0]/g, '')
+  const pokemonUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonId}`
+
+  fetch(pokemonUrl)
+   .then(response => response.json())
+   .then(data => {
+      const pokemon = data.pokemon
+      const form = data.form
+  })
+}
+
+
+window.removeEventListener("hashchange", loadPokemonDetails);
+window.addEventListener("hashchange", loadPokemonDetails);
