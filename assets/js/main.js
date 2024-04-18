1a69a022f5eb50ebed8670efa4b62ccb7981b32d
Gabriel Henrique <gabriel.henrique@agenciam3.com>
2024-04-17 22:08:19 -0300

    style: applying page styles and initing with js logic

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,14 +5,25 @@ function setUrlHash(hash = '1') {
 function loadPokemonDetails() {
   const pokemonId = window.location.hash.replace(/[#0]/g, '')
   const pokemonUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonId}`
+  console.log("aooba");
 
   fetch(pokemonUrl)
    .then(response => response.json())
    .then(data => {
-      const pokemon = data.pokemon
+      const pokemon = {}
       const form = data.form
   })
 }
+
+// function createPokemonStatus() {
+//   return `
+//   <div class="pokemon__base-infos">
+//     <span class="pokemon__status-name">HP</span>
+//     <span class="pokemon__status-number">45</span>
+//     <progress value="45" max="100"></progress>
+//   </div>
+//   `;
+// }
 
 
 window.removeEventListener("hashchange", loadPokemonDetails);
